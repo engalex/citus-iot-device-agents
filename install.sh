6ab4fed74199ba22c4fb11b4bee4c8b3fc566056
CuongQuay <cuong3ihut@gmail.com>
2016-09-22 11:41:56 +0700

    Update for docker build

--- install.sh
+++ install.sh
@@ -4,6 +4,3 @@ cp -r citus/ /usr/sbin/
 cp citus-device /usr/sbin/
 chmod +x /usr/sbin/citus-device
 chmod +x /usr/sbin/citus/*
-mkdir -p $HOME/.agent/
-echo "${DEVICE_ID}" >> ${HOME}/.agent/.device-id
-echo "${SECRET_KEY}" >> ${HOME}/.agent/.secret-key
