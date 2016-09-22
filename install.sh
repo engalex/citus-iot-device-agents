3403956403c49fc221df5ae6d2f02b7fbdcba69d
CuongQuay <cuong3ihut@gmail.com>
2016-09-22 11:39:09 +0700

    Update for docker build

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "Installing agents...."
-sudo cp -r citus/ /usr/sbin/
-sudo cp citus-device /usr/sbin/
-sudo chmod +x /usr/sbin/citus-device
-sudo chmod +x /usr/sbin/citus/*
+cp -r citus/ /usr/sbin/
+cp citus-device /usr/sbin/
+chmod +x /usr/sbin/citus-device
+chmod +x /usr/sbin/citus/*
 mkdir -p $HOME/.agent/
 echo "${DEVICE_ID}" >> ${HOME}/.agent/.device-id
 echo "${SECRET_KEY}" >> ${HOME}/.agent/.secret-key
