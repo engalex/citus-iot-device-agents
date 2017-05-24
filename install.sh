559b74de84a1e03f4091d9873ec14a6e9bedf515
CuongQuay <cuong3ihut@gmail.com>
2017-05-24 12:34:14 +0700

    Update install-sdk without pushd/popd

--- install.sh
+++ install.sh
@@ -6,18 +6,13 @@ BLUE='\033[1;34m'
 NC='\033[1;30m' # No Color
 DEF='\033[0m' # Default Color
 
-if ! [ -f /usr/sbin/citus-device ];
-then
-	echo -e "${BLUE}INFO: Installing citus-device CLI libraries....${NC}"
-	cp -r $HOME/citus-iot-device-agents/citus/ /usr/sbin/
-	cp $HOME/citus-iot-device-agents/citus-device /usr/sbin/
-	chmod +x /usr/sbin/citus-device
-	chmod +x /usr/sbin/citus/*
-	echo -e "${BLUE}INFO: Installed Software Agents successfully.${NC}"
-	echo -e "${DEF}"
-else
-	echo -e "${YELLOW}WARN: Citus Device Agents has already installed yet.${NC}"
-fi
+echo -e "${BLUE}INFO: Installing citus-device CLI libraries....${NC}"
+cp -r $HOME/citus-iot-device-agents/citus/ /usr/sbin/
+cp $HOME/citus-iot-device-agents/citus-device /usr/sbin/
+chmod +x /usr/sbin/citus-device
+chmod +x /usr/sbin/citus/*
+echo -e "${BLUE}INFO: Installed Software Agents successfully.${NC}"
+echo -e "${DEF}"
 
 if [ "${INSTALL_ALL}" = "YES" ]; then
 	citus-device activate
