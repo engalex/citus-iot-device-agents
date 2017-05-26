5a5819c3597872423d1f5fd7e4471eec1d879949
CuongQuay <cuong3ihut@gmail.com>
2017-05-26 10:17:39 +0700

    Update install cert by python

---
+++ citus/getcert.py
@@ -0,0 +1,14 @@
+import os
+import sys
+import json
+import requests
+
+try:
+    resp = requests.get(os.environ.get('SERVICE_ENDPOINT') + '/certificate/' + sys.argv[1], headers={'Authorization': 'Bearer ' + sys.argv[2]})
+    data = json.loads(resp.text)
+    with open(os.environ.get('HOME') + '/.agent/certs/' + os.environ.get('DEVICE_ID') + '.cert.pem', 'w') as f:
+        f.write(data['certificatePem'])
+    with open(os.environ.get('HOME') + '/.agent/certs/' + os.environ.get('DEVICE_ID') + '.private.key', 'w') as f:
+        f.write(data['keyPair']['PrivateKey'])
+except requests.exceptions.RequestException as e:
+    print 'Eror while retreiving certificate information: {}'.format(e)
