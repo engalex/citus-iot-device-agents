d6f820aa6137403b0f3c91f33f71a18b40c131cf
CuongQuay <cuong3ihut@gmail.com>
2017-05-26 11:46:12 +0700

    Update install cert by python

--- citus/setinfo.py
+++ citus/setinfo.py
@@ -9,14 +9,13 @@ try:
     data = json.loads(resp.text)
     GEO_LOCATION=data['address']
     GEO_LATITUDE=data['latitude']
-    GEO_LONGITUDE=data['longitude']
-    DEVICE_NAME='FPT-' + os.uname()[1]
-    data={'name':os.environ.get('DEVICE_ID'),'owner':sys.argv[1],'_metadata':{'name':DEVICE_NAME,'type': 'Default','location': GEO_LOCATION,'latitude': GEO_LATITUDE,'longitude': GEO_LONGITUDE,'lastModified':int(round(time.time() * 1000))}}    
+    GEO_LONGITUDE=data['longitude']    
+    data=sys.argv[1]
     headers = {'content-type': 'application/json'}
     params = {'secret_key':os.environ.get('SECRET_KEY')}
-    result = requests.post(os.environ.get('SERVICE_ENDPOINT') + '/apisrv/device-lifecycle-service/device/' + os.environ.get('DEVICE_ID') + '/_metadata', params=params, data=json.dumps(data), headers=headers)    
-    result.raise_for_status()
+    result = requests.post(os.environ.get('SERVICE_ENDPOINT') + '/apisrv/device-lifecycle-service/device/' + os.environ.get('DEVICE_ID') + '/_metadata', params=params, data=json.dumps(data), headers=headers)
     print result.__dict__
-    print data
+    print data    
+    result.raise_for_status()    
 except requests.exceptions.RequestException as e:
     print 'Eror while updating device information: {}'.format(e)
