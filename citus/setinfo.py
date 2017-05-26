29eb7c35256690499934cae0f1a2c8e51f2ddf56
CuongQuay <cuong3ihut@gmail.com>
2017-05-26 11:32:48 +0700

    Update install cert by python

--- citus/setinfo.py
+++ citus/setinfo.py
@@ -12,9 +12,11 @@ try:
     GEO_LONGITUDE=data['longitude']
     DEVICE_NAME='FPT-' + os.uname()[1]
     data={'name':os.environ.get('DEVICE_ID'),'owner':sys.argv[1],'_metadata':{'name':DEVICE_NAME,'type': 'Default','location': GEO_LOCATION,'latitude': GEO_LATITUDE,'longitude': GEO_LONGITUDE,'lastModified':int(round(time.time() * 1000))}}
+    print data.__dict__
     headers = {'content-type': 'application/json'}
     params = {'secret_key':os.environ.get('SECRET_KEY')}
-    result = requests.post(os.environ.get('SERVICE_ENDPOINT') + '/apisrv/device-lifecycle-service/device/' + os.environ.get('DEVICE_ID') + '/_metadata', params=params, data=json.dumps(data), headers=headers)
+    result = requests.post(os.environ.get('SERVICE_ENDPOINT') + '/apisrv/device-lifecycle-service/device/' + os.environ.get('DEVICE_ID') + '/_metadata', params=params, data=json.dumps(data), headers=headers)    
     result.raise_for_status()
+    print result.__dict__
 except requests.exceptions.RequestException as e:
     print 'Eror while updating device information: {}'.format(e)
