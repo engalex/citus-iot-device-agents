39cd29bf4015536182143b084183660e9aeba0f0
CuongQuay <cuong3ihut@gmail.com>
2017-05-26 10:41:21 +0700

    Update install cert by python

--- citus/getcert.py
+++ citus/getcert.py
@@ -5,6 +5,7 @@ import requests
 
 try:
     resp = requests.get(os.environ.get('SERVICE_ENDPOINT') + '/certificate/' + sys.argv[1], headers={'Authorization': 'Bearer ' + sys.argv[2]})
+    resp.raise_for_status()
     data = json.loads(resp.text)
     with open(os.environ.get('HOME') + '/.agent/certs/' + os.environ.get('DEVICE_ID') + '.cert.pem', 'w') as f:
         f.write(data['certificatePem'])
