6f74043b1ccff9c7c883a89d0034c8432bdf5a7d
CuongQuay <cuongdd1@Duongs-MBP-2.lan>
2017-07-05 11:17:22 +0700

    Update device-agents

--- install.sh
+++ install.sh
@@ -7,15 +7,18 @@ NC='\033[1;30m' # No Color
 DEF='\033[0m' # Default Color
 
 echo -e "${BLUE}INFO: Installing citus-device CLI libraries....${NC}"
-cp -r $HOME/citus-iot-device-installer/citus/ /usr/local/bin/
-cp $HOME/citus-iot-device-installer/citus-device /usr/local/bin/
+
 if [ -f /usr/bin/sudo ]; then
+	sudo cp -r $HOME/citus-iot-device-installer/citus/ /usr/local/bin/
+	sudo cp $HOME/citus-iot-device-installer/citus-device /usr/local/bin/
 	sudo chmod +x /usr/local/bin/citus-device
 	sudo chmod +x /usr/local/bin/citus/*
 	if [ ! -f /usr/bin/sw_vers ]; then
 		sudo ln -s /usr/local/bin/citus-device /usr/bin/citus-device
 	fi
 else
+	cp -r $HOME/citus-iot-device-installer/citus/ /usr/local/bin/
+	cp $HOME/citus-iot-device-installer/citus-device /usr/local/bin/
 	chmod +x /usr/local/bin/citus-device
 	chmod +x /usr/local/bin/citus/*
 	if [ ! -f /usr/bin/sw_vers ]; then
