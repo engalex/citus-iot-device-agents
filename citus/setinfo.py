8c85f07e9e2ccc0ec7d8ff4a8fa31a167215cafd
CuongQuay <cuongdd1@Duongs-MBP-2.lan>
2017-07-05 12:07:57 +0700

    Update device-agents

--- citus/setinfo.py
+++ citus/setinfo.py
@@ -13,7 +13,7 @@ try:
     DEVICE_NAME='FPT-' + os.uname()[1]
     TIMESTAMP=str(int(round(time.time() * 1000)))
     data={'name':os.environ.get('DEVICE_ID'),'owner':os.environ.get('DEVICE_OWNER'),'_metadata':{'name':DEVICE_NAME,'type': 'Default','location': GEO_LOCATION,'latitude': GEO_LATITUDE,'longitude': GEO_LONGITUDE,'lastModified': TIMESTAMP}}    
-    headers = {'content-type': 'application/json', }
+    headers = {'content-type': 'application/json'}
     params = '?owner=' + os.environ.get('DEVICE_OWNER') + '&secret_key=' + os.environ.get('SECRET_KEY')
     result = requests.put(os.environ.get('SERVICE_ENDPOINT') + '/apisrv/device-management-service/device/' + os.environ.get('DEVICE_ID') + '/_metadata' + params, data=json.dumps(data), headers=headers)    
     result.raise_for_status()    
