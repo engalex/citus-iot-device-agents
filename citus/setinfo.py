4efc4a820f00ad3edc1b75f6223acc841006902e
CuongQuay <cuongdd1@Duongs-MBP-2.lan>
2017-07-05 12:15:33 +0700

    Update device-agents

--- citus/setinfo.py
+++ citus/setinfo.py
@@ -12,10 +12,10 @@ try:
     GEO_LONGITUDE=str(data['longitude'])
     DEVICE_NAME='FPT-' + os.uname()[1]
     TIMESTAMP=str(int(round(time.time() * 1000)))
-    data={'name':os.environ.get('DEVICE_ID'),'owner':os.environ.get('DEVICE_OWNER'),'_metadata':{'name':DEVICE_NAME,'type': 'Default','location': GEO_LOCATION,'latitude': GEO_LATITUDE,'longitude': GEO_LONGITUDE,'lastModified': TIMESTAMP}}    
+    data={'name':os.environ.get('DEVICE_ID'),'owner':sys.argv[1],'_metadata':{'name':DEVICE_NAME,'type': 'Default','location': GEO_LOCATION,'latitude': GEO_LATITUDE,'longitude': GEO_LONGITUDE,'lastModified': TIMESTAMP}}    
     headers = {'content-type': 'application/json'}
-    params = '?owner=' + os.environ.get('DEVICE_OWNER') + '&secret_key=' + os.environ.get('SECRET_KEY')
-    result = requests.put(os.environ.get('SERVICE_ENDPOINT') + '/apisrv/device-management-service/device/' + os.environ.get('DEVICE_ID') + '/_metadata' + params, data=json.dumps(data), headers=headers)    
+    params = {'secret_key':os.environ.get('SECRET_KEY')}
+    result = requests.put(os.environ.get('SERVICE_ENDPOINT') + '/apisrv/device-management-service/device/' + os.environ.get('DEVICE_ID') + '/_metadata', params=params, data=json.dumps(data), headers=headers)    
     result.raise_for_status()    
 except requests.exceptions.RequestException as e:
     print 'Eror while updating device information: {}'.format(e)
